f89ece0e7267aa9b00d8ac301b8a9d6e0d993487
Alice Shao <a1ice_shao@yahoo.com>
2022-11-01 01:48:16 -0400

    updated method to create new array so code is shorter and updated prompt to properly include passwords up to 128 characters

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var uppercasing;
+var lowercasing;
+var numbers;
+var specialChar; 
+var temp = "";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +13,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Creates the password
@@ -21,7 +25,7 @@ function generatePassword() {
     alert("Password must be at least 8 characters.");
     generatePassword();
   } else if (passwordLength > 128){
-    alert("Password must be less than 128 characters.")
+    alert("Password must be 128 characters or less.")
     generatePassword();
   // If desired length is within range, next questions are asked and an array containing the wanted character types is created
   }else{
@@ -37,11 +41,6 @@ function generatePassword() {
   return generatedPassword;
 }
 
-var uppercasing;
-var lowercasing;
-var numbers;
-var specialChar; 
-
 // Asks the user about other password properties
 function questions(){
   uppercasing = confirm("Do you want to use uppercase letters in the password?");
@@ -50,64 +49,21 @@ function questions(){
   specialChar = confirm("Do you want to use special characters in the password?");
 }
 
-// Creates new array based on the questions
+// Creates and returns new array based on the questions
 function createNewArray(){
   var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
   var numberList = "0123456789"
   var specialList = "!@#$%^&*()[]:;\/|<>?,"
 
-  //1234
-  if(uppercasing && lowercasing && numbers && specialChar){
-    temp = uppercaseLetters + lowercaseLetters + numberList + specialList;
-  //123
-  }else if(uppercasing && lowercasing && numbers && !specialChar){
-    temp = uppercaseLetters + lowercaseLetters + numberList;
-  //12
-  }else if(uppercasing && lowercasing && !numbers && !specialChar){
-    temp = uppercaseLetters + lowercaseLetters;
-  //1
-  }else if(uppercasing && !lowercasing && !numbers && !specialChar){
-    temp = uppercaseLetters;
-  //124
-  }else if(uppercasing && lowercasing && !numbers && specialChar){
-    temp = uppercaseLetters + lowercaseLetters + specialList;
-  //14
-  }else if(uppercasing && !lowercasing && !numbers && specialChar){
-    temp = uppercaseLetters + specialList;
-  //4
-  }else if(!uppercasing && !lowercasing && !numbers && specialChar){
-    temp = specialList;
-  //134
-  }else if(uppercasing && !lowercasing && numbers && specialChar){
-    temp = uppercaseLetters + numberList + specialList;
-  //34
-  }else if(!uppercasing && !lowercasing && numbers && specialChar){
-    temp = numberList + specialList;
-  //13
-  }else if(uppercasing && !lowercasing && numbers && !specialChar){
-    temp = uppercaseLetters + numberList;
-  //234
-  }else if(!uppercasing && lowercasing && numbers && specialChar){
-    temp = lowercaseLetters + numberList + specialList;
-  //23
-  }else if(!uppercasing && lowercasing && numbers && !specialChar){
-    temp = lowercaseLetters + numberList;
-  //24
-  }else if(!uppercasing && lowercasing && !numbers && specialChar){
-    temp = lowercaseLetters + specialList;
-  //2
-  }else if(!uppercasing && lowercasing && !numbers && !specialChar){
-    temp = lowercaseLetters;
-  //3
-  }else if(!uppercasing && !lowercasing && numbers && !specialChar){
-    temp = numberList
-  }else{
-    alert("No password generated.")
-  }
+  if(uppercasing) temp += uppercaseLetters;
+  if(lowercasing) temp += lowercaseLetters;
+  if(numbers) temp += numberList;
+  if(specialChar) temp += specialList;
+  if(!uppercasing && !lowercasing && !numbers && !specialChar)
+    alert("No password generated.");
   return temp;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
